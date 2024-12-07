914192950e73304689aa32a97e522dd1be35237a
Krzysztof <janickik.23@gmail.com>
2024-12-08 00:18:14 +0100

    Added solution to a problem MergeStringsAlternately link: https://leetcode.com/problems/merge-strings-alternately/description/

--- Dynamic_Programming/Fibonacci_Number.py
+++ Dynamic_Programming/Fibonacci_Number.py
@@ -1,3 +1,6 @@
+#The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is
+# the sum of the two preceding ones, starting from 0 and 1. That is, Given n, calculate F(n).
+# link - https://leetcode.com/problems/fibonacci-number/description/
 class Solution:
     def fib(self, n: int) -> int:
         a = 0
@@ -5,9 +8,9 @@ class Solution:
         if n == 0:
             return 0
         if n == 1:
-            return 1
+            return 1 # this 2 cases are different we have do calculate them first
         else:
-            for i in range(1,n):
+            for i in range(1,n): # this loop is calculating our sum and then we are changing our variable b as the sum of previous elements and a is now previous element of b
                 c = a + b
                 a = b
                 b = c
---
+++ Linked_Lists/Linked List Cycle.py
@@ -0,0 +1,17 @@
+def remove_duplicates(self):
+    values = set()
+    previous = None
+    current = self.head
+
+    while current:
+        if current.value in values:
+            previous.next = current.next
+            self.length -= 1
+        else:
+            values.add(current.value)
+            previous = current
+            current = current.next
+    return False
+# Complexity
+#Time complexity: O(n)
+#Space complexity: O(1)
